3d9958f122c2e54cc223a13cf5f32df5d2003721
Valentin Ichkour <valentin.ichkour@epitech.eu>
2020-07-31 23:07:17 +0200

    Examples

---
+++ examples/class1.js
@@ -0,0 +1,16 @@
+const data1 = "data1"
+const data2 = "data2"
+
+/**
+ * this is MyClass
+ */
+export default class MyClass1 {
+  /**
+   * @param {number} a - this is a value.
+   * @param {number} b - this is a value.
+   * @return {number} result of the sum value.
+   */
+  sum(a, b) {
+    return a + b
+  }
+}
---
+++ examples/index.js
@@ -0,0 +1,16 @@
+const data1 = "data1"
+const data2 = "data2"
+
+/**
+ * this is MyClass
+ */
+export default class MyClass {
+  /**
+   * @param {number} a - this is a value a.
+   * @param {number} b - this is a value b.
+   * @return {number} result of the sum value.
+   */
+  sum(a, b) {
+    return a + b
+  }
+}
